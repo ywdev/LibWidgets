82462e95825ebdb325f94e91d9a9472de8e00afc
yaowang <appdev_yw@163.com>
2019-06-16 20:48:04 +0800

    CustomTitleBar

--- app/src/main/java/com/appdev/wsample/MainActivity.java
+++ app/src/main/java/com/appdev/wsample/MainActivity.java
@@ -2,6 +2,14 @@ package com.appdev.wsample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.appdev.lib.widgets.title.CommonTitleBar;
+import com.appdev.lib.widgets.title.listener.OnMoreActionItemClickListener;
+import com.appdev.lib.widgets.title.model.MoreAction;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<MoreAction> moreActions = new ArrayList<>();
+        moreActions.add(new MoreAction("H",R.drawable.icon_title_search));
+        moreActions.add(new MoreAction("B",R.drawable.icon_title_add));
+        CommonTitleBar commonTitleBar = findViewById(R.id.ctb_title_bar);
+        commonTitleBar.setActions(moreActions, new OnMoreActionItemClickListener() {
+            @Override
+            public void OnMoreActionItemClick(MoreAction moreAction, View view, int position) {
+
+            }
+        });
     }
 }
